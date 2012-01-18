157450ebaa1e8605dfc6d403c1498faa658f7f31
Sam Wurzel <sam@octopart.com>
2012-01-18 11:46:03 -0800

    Add Thrift to dependencies, bump version

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ setup(
     author='Jeremy Jones',
     author_email='jeremyj@letifer.org',
     url='http://github.com/jmj/ScribeHandler',
-    version='0.052',
+    version='0.053',
     py_modules=['ScribeHandler'],
     license='GPLv2',
     long_description=open('README').read(),
     description='ScribeHandler is a simple proxy layer that works with the python standard logging module',
     install_requires = [
-        "scribe>=2.0",
+        'scribe>=2.0',
+        'Thrift>=0.8.0utf',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
